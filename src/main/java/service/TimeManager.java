4e42789e5b89675a8e2bf685077b87971144640a
hyebim <shb010424@gmail.com>
2025-08-08 14:35:42 +0900

    thread class 수정

---
+++ src/main/java/service/TimeManager.java
@@ -0,0 +1,49 @@
+package service;
+
+import types.Time;
+
+public class TimeManager {
+    private GameState gameState;
+
+    public TimeManager(GameState gameState) {
+        this.gameState = gameState;
+    }
+
+    public void advanceTime() {
+        Time current = gameState.getCurrentTime();
+        Time next;
+
+        switch (current) {
+            case Morning:
+                next = Time.Afternoon;
+                break;
+            case Afternoon:
+                next = Time.Evening;
+                break;
+            default:
+                next = Time.Morning;
+                break;
+        }
+
+        gameState.setCurrentTime(next);
+        printTimeMessage(next);
+    }
+
+    public void printTimeMessage(Time time) {
+        switch (time) {
+            case Morning:
+                System.out.println("\n[아침이 되었습니다.]");
+                System.out.println("밤 사이에 한 명이 죽었습니다...");
+                break;
+            case Afternoon:
+                System.out.println("\n[오후입니다.]");
+                System.out.println("용의자를 지목하여 죽이거나 피의자의 정보를 알 수 있습니다.");
+                break;
+            case Evening:
+                System.out.println("\n[밤입니다.]");
+                System.out.println("지금은 아무 행동도 할 수 없습니다. 조용히 밤을 보내세요...");
+                break;
+        }
+    }
+}
+
